2de00a7344f4a97629355ee5b1e33fd0ee5f93d3
Himanshu Chopra <himanshuchopra2008@outlook.com>
2020-08-26 20:40:57 +0530

    Final commit after Part 8

--- Blog Web/Blog_Web/settings.py
+++ Blog Web/Blog_Web/settings.py
@@ -114,6 +114,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
